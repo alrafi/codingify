d333af2becaebcb71de38166be9432afb5193e58
alrafi <hafisalrafi@gmail.com>
2020-05-10 13:08:41 +0700

    add adaptive learning on quiz

--- src/pages/Quiz/component/Algoritma.js
+++ src/pages/Quiz/component/Algoritma.js
@@ -15,6 +15,8 @@ import 'ace-builds/src-noconflict/mode-java';
 import 'ace-builds/src-noconflict/theme-github';
 import { SubTopic, CodeText } from '../../../components/contentComponent';
 import check from '../../../assets/check.png';
+import rightArrow from '../../../assets/right-arrow.png';
+import adaptive from '../../../assets/adaptive.png';
 
 const TitleEditor = styled.p`
   font-size: 120%;
@@ -60,14 +62,63 @@ const ModalBox = styled.div`
   }
 `;
 
+const NextWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  img {
+    margin-left: 10px;
+    width: 30px;
+    height: 30px;
+  }
+`;
+
 const TopicDone = styled.p`
-  font-size: 220%;
+  font-size: 200%;
   font-weight: bold;
 `;
 
 const TopicName = styled.p`
   font-size: 180%;
   margin: 10px 0;
+`;
+
+// adaptive modal
+const AdaptiveText = styled.p`
+  font-size: 160%;
+  font-weight: bold;
+`;
+
+const AdaptiveNext = styled.p`
+  font-size: 140%;
+  margin: 10px 0;
+`;
+
+const AdaptiveWrapper = styled.div`
+  display: flex;
+
+  img {
+    width: 100px;
+    height: 280px;
+  }
+`;
+
+const AdaptiveContainer = styled.div`
+  margin-left: 10px;
+`;
+
+const ChooseWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const OrText = styled.p`
+  font-size: 100%;
+  text-align: center;
+  margin: 20px 0;
 `;
 
 const CloseWrapper = styled.div`
@@ -101,10 +152,17 @@ const Algoritma = () => {
     `);
   };
 
-  const [code, setCode] = useState(`// setup
-let myArr = [];
+  const [code, setCode] = useState(`// Setup
+const rotasiMatriks = (m, n, matriks) => {
+  // Only change code below this line
 
-// only change code below this line
+  for (let i = 0; i < m; i++) {
+    arr = matriks[i];
+  }
+  return arr;
+
+  // Only change code above this line
+}; 
 `);
 
   const onChange = (newValue) => {
@@ -121,6 +179,8 @@ let myArr = [];
   const closeModal = () => {
     setIsOpen(false);
   };
+
+  const modalLogic = () => true;
 
   return (
     <MainWrapper>
@@ -161,22 +221,58 @@ let myArr = [];
           <Button onClick={openModal}>SUBMIT</Button>
         </ButtonWrapper>
       </ConsoleSection>
-      <Modal
-        isOpen={modalIsOpen}
-        onRequestClose={closeModal}
-        style={customStyles}
-        contentLabel="Materi selesai"
-      >
-        <CloseWrapper>
-          <ModalClose onClick={closeModal}>X</ModalClose>
-        </CloseWrapper>
-        <ModalBox>
-          <TopicDone>Materi Selesai</TopicDone>
-          <img src={check} alt="completed" />
-          <TopicName>Array - Perkenalan</TopicName>
-          <Button width="200px">Lanjut Mengerjakan Kuis</Button>
-        </ModalBox>
-      </Modal>
+      {!modalLogic ? (
+        <Modal
+          isOpen={modalIsOpen}
+          onRequestClose={closeModal}
+          style={customStyles}
+          contentLabel="Materi selesai"
+        >
+          <CloseWrapper>
+            <ModalClose onClick={closeModal}>X</ModalClose>
+          </CloseWrapper>
+          <ModalBox>
+            <TopicDone>Selamat! Jawaban Anda benar</TopicDone>
+            <img src={check} alt="completed" />
+            <NextWrapper>
+              <TopicName>Kuis Selanjutnya: Wildcard</TopicName>
+              <img src={rightArrow} alt="next" />
+            </NextWrapper>
+            <Button width="200px">Kembali ke Halaman Kuis</Button>
+          </ModalBox>
+        </Modal>
+      ) : (
+        <Modal
+          isOpen={modalIsOpen}
+          onRequestClose={closeModal}
+          style={customStyles}
+          contentLabel="Adaptive Learning"
+        >
+          <CloseWrapper>
+            <ModalClose onClick={closeModal}>X</ModalClose>
+          </CloseWrapper>
+          <ModalBox>
+            <AdaptiveText>Ups! Sepertinya ada yang ketinggalan</AdaptiveText>
+
+            <AdaptiveWrapper>
+              <img src={adaptive} alt="Adaptive Learning" />
+              <AdaptiveContainer>
+                <ChooseWrapper>
+                  <AdaptiveNext>Kembali belajar materi Array?</AdaptiveNext>
+                  <Button width="200px">Belajar Sekarang</Button>
+                </ChooseWrapper>
+                <OrText>ATAU</OrText>
+                <ChooseWrapper>
+                  <AdaptiveNext>
+                    Coba kuis lain: Bebek Pak Dangklek
+                  </AdaptiveNext>
+                  <Button width="200px">Kerjakan kuis ini</Button>
+                </ChooseWrapper>
+              </AdaptiveContainer>
+            </AdaptiveWrapper>
+          </ModalBox>
+        </Modal>
+      )}
     </MainWrapper>
   );
 };
