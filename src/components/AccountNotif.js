d8adfaa31a911877e4e2e19ff301636706e81f45
alrafi <hafisalrafi@gmail.com>
2020-06-01 00:52:42 +0700

    fix logout button

--- src/components/AccountNotif.js
+++ src/components/AccountNotif.js
@@ -2,17 +2,22 @@ import React from 'react';
 import styled from 'styled-components';
 import notif from '../assets/notif.png';
 import user from '../assets/user.png';
-import downArrow from '../assets/down-arrow.png';
+import logout from '../assets/logout.png';
+import { Link } from 'react-router-dom';
 
 const AccountNotifStyled = styled.div`
   display: flex;
   justify-content: flex-end;
   margin-bottom: 50px;
+
+  .link {
+    text-decoration: none;
+  }
 `;
 
 const AccountControl = styled.div`
   display: flex;
-  padding: 0 15px;
+  padding: 5px 15px;
   justify-content: space-between;
   align-items: center;
   background: #009d86;
@@ -40,15 +45,22 @@ const AccountNotif = () => {
           cursor: 'pointer',
         }}
       />
-      <AccountControl>
-        <img src={user} alt="user" style={{ width: '23px', height: '21px' }} />
-        <p>alrafi</p>
-        <img
-          src={downArrow}
-          alt="open"
-          style={{ width: '14px', height: '5px' }}
-        />
-      </AccountControl>
+      <Link to="/" className="link">
+        <AccountControl>
+          <img
+            src={user}
+            alt="user"
+            style={{ width: '23px', height: '21px' }}
+            onCLick={() => console.log('hi')}
+          />
+          <p>logout</p>
+          <img
+            src={logout}
+            alt="open"
+            style={{ marginLeft: '5px', width: '14px', height: '14px' }}
+          />
+        </AccountControl>
+      </Link>
     </AccountNotifStyled>
   );
 };
